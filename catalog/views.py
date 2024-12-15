c47c947aec92fdfb1921ade561ada3ee6eef661e
Almaz207 <muzafarov.almaz2012@yandex.ru>
2024-12-16 03:16:09 +0400

    create custom privet

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseForbidden, request
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from catalog.forms import ProductForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product
 
 
@@ -11,6 +13,10 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:catalog_list')
+
+    def form_valid(self, form):
+        form.object.owner = self.request.user
+        return form.object.owner
 
 
 class ProductListView(ListView):
@@ -30,8 +36,17 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:catalog_list')
 
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm("catalog.change_product"):
+            return ProductModeratorForm
+        raise PermissionDenied
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+
+class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy('catalog:catalog_list')
+    permission_required = 'catalog.delete_product'
